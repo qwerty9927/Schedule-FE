4b9ecaeb5b74babbf01bd2d5b83e4926ef2c6c2a
qwerty9927 <tanvo9927@gmail.com>
2022-11-28 15:09:37 +0700

    change api url

--- src/components/core/Footer.js
+++ src/components/core/Footer.js
@@ -4,7 +4,6 @@ function Footer() {
   
   return (
     <div className="footer">
-      <p><span style={{color: "#fff", fontWeight: "bold"}}>Report to </span>9927.qwerty@gmail.com</p>
       <p>&#169; 2022 by Qwerty9927</p>
     </div>
   )
