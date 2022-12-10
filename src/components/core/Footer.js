7024d787239528ee4996b1e592db6c29b613288f
qwerty9927 <tanvo9927@gmail.com>
2022-12-10 18:42:05 +0700

    add description

--- src/components/core/Footer.js
+++ src/components/core/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <div className="footer">
       <p style={{color: "var(--secondary)", fontWeight: "bold"}}>Trang web sắp dừng hoạt động hãy chụp lại thời khóa biểu của mình</p>
+      <p>Trang web chỉ mang tính chất tham khảo</p>
       <p>&#169; 2022 by Qwerty9927</p>
     </div>
   )
