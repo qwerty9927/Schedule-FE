76836e7b937a7fb915a839e9132bf0761ae6f8dc
qwerty9927 <tanvo9927@gmail.com>
2022-12-11 16:59:48 +0700

    edit license

--- src/components/core/Footer.js
+++ src/components/core/Footer.js
@@ -4,7 +4,6 @@ function Footer() {
   
   return (
     <div className="footer">
-      <p style={{color: "var(--secondary)", fontWeight: "bold"}}>Trang web sắp dừng hoạt động hãy chụp lại thời khóa biểu của mình</p>
       <p>Trang web chỉ mang tính chất tham khảo</p>
       <p>&#169; 2022 by Qwerty9927</p>
     </div>
