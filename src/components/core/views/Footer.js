3ee8c0295715fd731d4206df14ec9d3293d676c8
qwerty9927 <tanvo9927@gmail.com>
2022-12-29 20:54:35 +0700

    incognito

--- src/components/core/views/Footer.js
+++ src/components/core/views/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div className="footer" id="footer">
       <p>Chức năng đăng ký môn học chỉ mang tính chất tham khảo</p>
       <p style={{fontWeight: "bold", color: "var(--secondary)"}}>Tìm hiểu rõ môn tiên quyết trước khi sử dùng</p>
-      <p>&#169; 2022 by Qwerty9927</p>
+      <p>&#169; 2022</p>
     </div>
   )
 }
