9a06a3883f11b955ed55637c7bf9ee44ad225c35
qwerty9927 <tanvo9927@gmail.com>
2023-02-04 23:25:29 +0700

    init is notfound

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Core from './components/core/index'
 import NotFound from './components/core/views/NotFound'
 
 function App() {
+  alert("Hiện tại phòng học đã bị thay đổi không còn chính xác. \nXin quay lại sau khi thông báo này mất!")
   return (
     // <Core />
     <NotFound />
